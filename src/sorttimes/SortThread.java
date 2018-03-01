aea4f20652ceb36f0e68f956c9a143b556bb5c53
Juan Chacon <johnspartanx86@gmail.com>
2018-02-28 19:09:00 -0700

    Ya jala, pero no son las mejores practicas jeje

--- src/sorttimes/SortThread.java
+++ src/sorttimes/SortThread.java
@@ -8,6 +8,7 @@ package sorttimes;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
+import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 /**
@@ -23,8 +24,11 @@ public class SortThread extends Thread{
     
     int Kind; // 1 QuickSort, 2 Bubblesort, 3 shellSort
     int[] Array;
+    boolean State;
     JTextField ShowBoxTime;
     JTextPane ShowBoxArray;
+    String time;
+    frmShow forma;
     /**
      * 
      * @param A Array to sort
@@ -32,14 +36,29 @@ public class SortThread extends Thread{
      * @param tTiempo JTextField were the time is going to display.
      * @param tMostrar JTextPane were the sorted array is going to be displayed.
      */
-    public SortThread(int[] A, int Metodo, JTextField tTiempo, JTextPane tMostrar)
+    public SortThread(int[] A, int Metodo, JTextField tTiempo, JTextPane tMostrar, frmShow frm)
     {
         Kind = Metodo;
         Array =A;
         ShowBoxTime =tTiempo;
         ShowBoxArray = tMostrar;
+        State=false;
+        forma = frm;
+    }
+    public int GetKind()
+    {
+        return Kind;
     }
     
+    public boolean GetState()
+    {
+        return State;
+    }
+    
+    public String GetTime()
+    {
+       return time;
+    }
     
     
     @Override
@@ -61,9 +80,10 @@ public class SortThread extends Thread{
                 
         }
         Instant end = Instant.now();
-        String time = Duration.between(start, end).toString();
+        
+        time = Duration.between(start, end).toString();
+        forma.Fastest(ShowBoxTime,time);
         ShowBoxTime.setText(time);
         ShowBoxArray.setText(Arrays.toString(Array));
-        
     }
 }
